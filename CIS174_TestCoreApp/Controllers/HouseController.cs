afefe0c272f1252fa020f9169ebb9d5825373578
WDB40 <wbrown1640@gmail.com>
2019-10-11 22:25:48 -0400

    Completing 8.1

---
+++ CIS174_TestCoreApp/Controllers/HouseController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CIS174_TestCoreApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CIS174_TestCoreApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HouseController : ControllerBase
+    {
+        public IEnumerable<HouseViewModel> Houses = new List<HouseViewModel>() {
+            new HouseViewModel(1, 4, 1854, new DateTime(1973, 5, 28), "Carpet"),
+            new HouseViewModel(2, 3, 1675, new DateTime(2015, 10, 17), "Hardwood")
+        };
+
+        [HttpGet]
+        public IActionResult AllHouses()
+        {
+            return Ok(Houses);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetHouse(int id)
+        {
+            foreach(HouseViewModel house in Houses)
+            {
+                if(house.Id == id)
+                {
+                    return Ok(house);
+                }
+            }
+
+            return NotFound();
+        }
+        [HttpPost("create")]
+        public HouseViewModel AddHouse([FromBody] HouseViewModel house)
+        {
+            List<HouseViewModel> houseList = Houses.ToList();
+            houseList.Add(house);
+            Houses = houseList;
+
+            Response.StatusCode = StatusCodes.Status201Created;
+
+            return house;
+        }
+    }
+}
---
+++ CIS174_TestCoreApp/Models/HouseViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CIS174_TestCoreApp.Models
+{
+    public class HouseViewModel
+    {
+        public int Id { get; set; }
+        
+        public int Bedrooms { get; set; }
+
+        public double SquareFeet { get; set; }
+
+        public DateTime DateBuilt { get; set; }
+
+        public string Flooring { get; set; }
+
+        public HouseViewModel(int Id, int Bedrooms, double SquareFeet, DateTime DateBuilt, string Flooring)
+        {
+            this.Id = Id;
+            this.Bedrooms = Bedrooms;
+            this.SquareFeet = SquareFeet;
+            this.DateBuilt = DateBuilt;
+            this.Flooring = Flooring;
+        }
+    }
+}
